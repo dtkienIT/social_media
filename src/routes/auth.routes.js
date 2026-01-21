a177a338c0cafa5fdb79ef148af23a3e27369429
Kien Do <dotrungkien1382003@gmail.com>
2026-01-21 23:22:42 +0700

    feat: add cloudinary image upload

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/auth.controller');
+const uploadCloud = require('../middlewares/upload');
 
+router.post('/register', uploadCloud.single('avatar'), authController.register);
 router.post('/register', register);
 router.post('/login', login);
 
