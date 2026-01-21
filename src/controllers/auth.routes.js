81f67a3e1beca312995e1a7df07d62bbf7e7fbb5
Kien Do <dotrungkien1382003@gmail.com>
2026-01-21 22:45:31 +0700

    delete controllers

--- src/controllers/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth.controller');
-
-// Đường dẫn sẽ là: /api/auth/register
-router.post('/register', authController.register);
-
-module.exports = router;
