3fe8998186b672cb7d0c1e080072cdebc111655b
Kien Do <dotrungkien1382003@gmail.com>
2026-01-22 15:24:12 +0700

    update code

--- src/models/index.js
+++ src/models/index.js
@@ -1,22 +1,17 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('./Comment'); // Model bạn vừa tạo
+const Comment = require('./Comment');
 
-// --- QUAN HỆ USER - POST ---
-User.hasMany(Post, { foreignKey: 'userId', as: 'posts' });
-Post.belongsTo(User, { foreignKey: 'userId', as: 'User' });
+// Quan hệ User - Post 
+User.hasMany(Post, { foreignKey: 'userId' });
+Post.belongsTo(User, { foreignKey: 'userId' });
 
-// --- QUAN HỆ USER - COMMENT ---
-User.hasMany(Comment, { foreignKey: 'userId', as: 'comments' });
-Comment.belongsTo(User, { foreignKey: 'userId', as: 'User' });
+// Quan hệ User - Comment 
+User.hasMany(Comment, { foreignKey: 'userId' });
+Comment.belongsTo(User, { foreignKey: 'userId' });
 
-// --- QUAN HỆ POST - COMMENT ---
-// Khi xóa bài viết (Post), tất cả bình luận (Comment) liên quan sẽ bị xóa theo (CASCADE)
-Post.hasMany(Comment, { foreignKey: 'postId', as: 'postComments', onDelete: 'CASCADE' });
+// Quan hệ Post - Comment
+Post.hasMany(Comment, { foreignKey: 'postId', onDelete: 'CASCADE' });
 Comment.belongsTo(Post, { foreignKey: 'postId' });
 
-module.exports = {
-  User,
-  Post,
-  Comment
-};
+module.exports = { User, Post, Comment };
