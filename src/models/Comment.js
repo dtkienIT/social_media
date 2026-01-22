8d80f0f0e0e86c436a475703150cd3cda4c66d22
Kien Do <dotrungkien1382003@gmail.com>
2026-01-22 15:17:37 +0700

    update code

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,24 +1,24 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { sequelize } = require('../config/database'); 
 
 const Comment = sequelize.define('Comment', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
-  },
-  content: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  userId: {
-    type: DataTypes.UUID,
-    allowNull: false
-  },
-  postId: {
-    type: DataTypes.UUID,
-    allowNull: false
-  }
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    userId: {
+        type: DataTypes.UUID,
+        allowNull: false
+    },
+    postId: {
+        type: DataTypes.UUID,
+        allowNull: false
+    }
 });
 
 module.exports = Comment;
